cd05e268f02f8890b1b475ebcaa79e3a0b62b7f8
elenaaasky <dengjie_biz@foxmail.com>
2026-04-24 16:37:41 +0800

    Polish launcher dialogs

--- src/CPALauncher.Wpf/Views/LauncherDialog.cs
+++ src/CPALauncher.Wpf/Views/LauncherDialog.cs
@@ -5,12 +5,20 @@ namespace CPALauncher.Views;
 
 public static class LauncherDialog
 {
+    public const string NotificationToken = "MainWindowNotification";
+
     public static MessageBoxResult Show(
         string messageBoxText,
         string caption,
         MessageBoxButton button,
         MessageBoxImage icon)
     {
+        if (button == MessageBoxButton.OK)
+        {
+            ShowNotification(messageBoxText, caption, icon);
+            return MessageBoxResult.OK;
+        }
+
         var dialog = new LauncherDialogWindow(caption, messageBoxText, button, icon);
         var owner = Application.Current?.MainWindow;
         if (owner != null && owner.IsLoaded)
@@ -29,6 +37,51 @@ public static class LauncherDialog
             info.Caption,
             info.Button,
             ResolveImage(info));
+    }
+
+    private static void ShowNotification(string message, string caption, MessageBoxImage icon)
+    {
+        var app = Application.Current;
+        if (app?.Dispatcher == null)
+            return;
+
+        app.Dispatcher.Invoke(() =>
+        {
+            var growlInfo = new GrowlInfo
+            {
+                Message = string.IsNullOrWhiteSpace(caption) ? message : $"{caption}\n{message}",
+                Token = NotificationToken,
+                WaitTime = ResolveWaitTime(icon),
+                ShowDateTime = false,
+                ShowCloseButton = false,
+            };
+
+            switch (icon)
+            {
+                case MessageBoxImage.Error:
+                    HandyControl.Controls.Growl.Error(growlInfo);
+                    break;
+                case MessageBoxImage.Warning:
+                    HandyControl.Controls.Growl.Warning(growlInfo);
+                    break;
+                case MessageBoxImage.Information:
+                    HandyControl.Controls.Growl.Info(growlInfo);
+                    break;
+                default:
+                    HandyControl.Controls.Growl.Info(growlInfo);
+                    break;
+            }
+        });
+    }
+
+    private static int ResolveWaitTime(MessageBoxImage icon)
+    {
+        return icon switch
+        {
+            MessageBoxImage.Error => 8,
+            MessageBoxImage.Warning => 6,
+            _ => 4,
+        };
     }
 
     private static MessageBoxResult NormalizeResult(MessageBoxResult result, MessageBoxButton button)
